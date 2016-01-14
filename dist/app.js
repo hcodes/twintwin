95f027c69760e906dbf54c757f990eab8cd3985e
Denis Seleznev <hcodes@yandex.ru>
2016-01-14 11:16:54 +0300

    Rebuild

--- dist/app.js
+++ dist/app.js
@@ -47,6 +47,31 @@ var setCSS = function(el, props) {
     });
 };
 
+// for iPad 1
+if(!Function.prototype.bind) {
+    Function.prototype.bind = function(oThis) {
+        if(typeof this !== 'function') {
+            throw new TypeError('Function.prototype.bind - what is trying to be bound is not callable');
+        }
+
+        var aArgs = Array.prototype.slice.call(arguments, 1),
+            fToBind = this,
+            NOP = function() {
+            },
+            fBound = function() {
+                return fToBind.apply(this instanceof NOP && oThis
+                        ? this
+                        : oThis,
+                    aArgs.concat(Array.prototype.slice.call(arguments)));
+            };
+
+        NOP.prototype = this.prototype;
+        fBound.prototype = new NOP();
+
+        return fBound;
+    };
+}
+
 var App = {
     init: function() {
         this.page.show('main');
